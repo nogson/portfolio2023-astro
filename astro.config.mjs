3d081dbfc470b9e9cf12fa7f233643f66c04e3c4
佐藤　文彦 <fumihiko_sato@post.pioneer.co.jp>
2023-08-15 10:28:02 +0900

    初回コミット

---
+++ astro.config.mjs
@@ -0,0 +1,26 @@
+import { defineConfig } from 'astro/config';
+import vercel from "@astrojs/vercel/serverless";
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
+
+import image from "@astrojs/image";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  adapter: vercel(),
+  integrations: [react(), mdx(), image()] ,
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: [
+            `@use "src/styles/variables.scss" as *; \
+            @use "src/styles/common.scss" as *;`,
+          ]
+        }
+      }
+    }
+  },
+
+});
