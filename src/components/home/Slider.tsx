3cb50897a5c5ccf5facaf829a201922bf992ce44
佐藤　文彦 <fumihiko_sato@post.pioneer.co.jp>
2023-08-24 09:14:08 +0900

    スライダー実装

--- src/components/home/Slider.tsx
+++ src/components/home/Slider.tsx
@@ -1,4 +1,59 @@
 import styled from "styled-components";
+import { Transition } from "react-transition-group";
+import { useEffect, useState } from "react";
+
+type Props = {
+  images: string[];
+};
+
+const Slider = ({ images }: Props) => {
+  const windowWidth = window.innerWidth;
+  const itemWidth = windowWidth / 2;
+  const sliderListWidth = itemWidth * images.length;
+  const duration = 300;
+  const [startX, setStartX] = useState(-sliderListWidth / 2 + itemWidth / 2);
+  const [items, setItems] = useState<any>([]);
+
+  useEffect(() => {
+    const thumbnails = images.map((image: string) => {
+      // TODO @で指定しえいるrootエイリアスをどうにかできないか
+      return import(image.replace("@/", "../../"));
+    });
+    const getImages = async () => {
+      const res = await Promise.all(thumbnails);
+      setItems(res.map((d) => d.default.src));
+    };
+    getImages();
+  }, [images]);
+
+  const back = () => {
+    console.log("back");
+    setStartX(startX + itemWidth);
+  };
+
+  const next = () => {
+    console.log("next");
+    setStartX(startX - itemWidth);
+  };
+
+  return (
+    <SliderWrapper>
+      <BackButton onClick={back} type="button">
+        左
+      </BackButton>
+      <Transition timeout={duration}>
+        <SliderList $width={sliderListWidth} $startX={startX}>
+          {items.map((item: any, index: number) => (
+            <SliderItem key={index} $width={itemWidth}>
+              <img src={item} alt="" />
+            </SliderItem>
+          ))}
+        </SliderList>
+      </Transition>
+      <NextButton onClick={next}>右</NextButton>
+    </SliderWrapper>
+  );
+};
 
 const SliderWrapper = styled.div`
   width: 100%;
@@ -9,72 +64,43 @@ const SliderWrapper = styled.div`
   transform: translateZ(0);
 `;
 
-const SliderList = styled.div<{ $width: number,$startX: number }>`
+const SliderList = styled.div<{ $width: number; $startX: number }>`
   width: ${({ $width }) => $width}px;
   height: 100%;
   position: relative;
-  display:block;
+  display: block;
+  transition: 0.5s;
   transform: ${({ $startX }) => `translate3d(${$startX}px, 0px, 0px)`};
-  transition: all 1.0s;
 `;
 
 const SliderItem = styled.div<{ $width: number }>`
   width: ${({ $width }) => $width}px;
-  height: 100px;
   float: left;
   min-height: 1px;
   box-sizing: border-box;
-  border: 1px solid #000;
-  `;
+  padding: var(--spacing-XL) var(--spacing-XL);
+  img {
+    width: 100%;
+    height: auto;
+    border-radius: 50px;
+    box-shadow: var(--shadow-02);
+  }
+`;
 
 const BaseButton = styled.button`
-    position: absolute;
-    top:50%;
-    transform: translateY(-50%);
-    z-index: 10;
-    cursor: pointer;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  z-index: 10;
+  cursor: pointer;
 `;
 
 const BackButton = styled(BaseButton)`
-    left: 10px;
+  left: 10px;
 `;
 
 const NextButton = styled(BaseButton)`
   right: 10px;
 `;
-
-
-const Slider = () => {
-  const windowWidth = window.innerWidth;
-  const item = [1, 2, 3, 4];
-  const itemWidth = windowWidth / 2;
-  const sliderListWidth = itemWidth * item.length;
-  let startX = -itemWidth;
-
-  const back = () => {
-    console.log('back')
-    startX = startX + itemWidth;
-  }
-
-  const next = () => {
-    console.log('next')
-    startX = startX - itemWidth;
-  }
-
-  return (
-    <SliderWrapper>
-      <BackButton onClick={back} type="button">左</BackButton>
-      <SliderList $width={sliderListWidth} $startX={startX}>
-        {item.map((item, index) => (
-          <SliderItem key={index} $width={itemWidth}>
-            {item}
-          </SliderItem>
-        ))}
-      </SliderList>
-      <NextButton onClick={next}>右</NextButton>
-    </SliderWrapper>
-  );
-};
-
 
 export default Slider;
