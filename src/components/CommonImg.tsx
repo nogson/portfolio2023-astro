527bec8e7677cb444184e38468ec14a7b6210537
佐藤　文彦 <satofaction@gmail.com>
2023-10-07 19:27:39 +0900

    ぶろぐ追加

--- src/components/CommonImg.tsx
+++ src/components/CommonImg.tsx
@@ -2,22 +2,47 @@ import { useEffect, useRef, useState } from "react";
 import { styled } from "styled-components";
 import { IoCloseSharp } from "react-icons/io5";
 
-export default function CommonImg({ src, alt }: { src: string; alt: string }) {
+export default function CommonImg({
+  src,
+  alt,
+  width,
+}: {
+  src: string;
+  alt: string;
+  width?: string;
+}) {
   const [isShow, setIsShow] = useState(false);
+  const [imgW, setImgW] = useState<string>("100px");
+  const imgElm = useRef<HTMLImageElement>(null);
 
   const openWindow = () => {
     setIsShow(!isShow);
   };
 
+  useEffect(() => {
+    const threshold = window.innerWidth - 200;
+    const w =
+      imgElm.current && threshold > imgElm.current.naturalWidth
+        ? imgElm.current.naturalWidth
+        : threshold;
+    setImgW(`${w}px`);
+  });
+
   return (
     <div>
       <Img src={src} alt={alt} onClick={openWindow} />
-      <Dialog $isShow={isShow} onClick={openWindow}>
+      <Dialog $isShow={isShow} $imgWidth={imgW} onClick={openWindow}>
         <div className="dialog">
           <div className="close-button">
             <IoCloseSharp />
           </div>
-          <img src={src} alt={alt} onClick={(e) => e.stopPropagation()} />
+          <img
+            src={src}
+            alt={alt}
+            width={width}
+            onClick={(e) => e.stopPropagation()}
+            ref={imgElm}
+          />
         </div>
       </Dialog>
     </div>
@@ -28,7 +53,7 @@ const Img = styled.img`
   cursor: pointer;
 `;
 
-const Dialog = styled.div<{ $isShow: boolean }>`
+const Dialog = styled.div<{ $isShow: boolean; $imgWidth: string }>`
   position: fixed;
   top: 0;
   left: 0;
@@ -62,6 +87,9 @@ const Dialog = styled.div<{ $isShow: boolean }>`
       padding: var(--spacing-M);
       background: var(--color-white);
       border-radius: 100%;
+    }
+    img {
+      width: ${({ $imgWidth }) => $imgWidth};
     }
   }
 
