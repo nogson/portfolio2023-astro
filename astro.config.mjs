a774fbfa9f23ceddbc2124ebd7439d6afdb27f40
佐藤　文彦 <satofaction@gmail.com>
2023-10-15 11:00:23 +0900

    GAタグ追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,36 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 import image from "@astrojs/image";
-
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-    // github pages hosting
+  // github pages hosting
   // site: 'https://nogson.github.io/',
   // base: '/portfolio2023-astro',
-  integrations: [react(), mdx(), image()],
+  integrations: [ 
+    react(),
+    mdx(),
+    image(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   vite: {
     ssr: {
       noExternal: ["react-icons"],
     },
     resolve: {
       alias: {
-        "@": "/src"
-      }
+        "@": "/src",
+      },
     },
     css: {
       preprocessorOptions: {
@@ -28,10 +38,10 @@ export default defineConfig({
           additionalData: [
             `@use "src/styles/variables.scss" as *; \
             @use "src/styles/common.scss" as *; \
-            @use "src/styles/detailPageLayout.scss" as *;`
-          ]
-        }
-      }
-    }
+            @use "src/styles/detailPageLayout.scss" as *;`,
+          ],
+        },
+      },
+    },
   },
 });
